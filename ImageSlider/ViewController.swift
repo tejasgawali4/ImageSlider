da25ac4476831d7b4d9d8d53b589e5e35b150192
Tejas Gawali <tejasgawali@Tejass-Mac.local>
2018-04-01 01:18:06 -0700

    imageslider

--- ImageSlider/ViewController.swift
+++ ImageSlider/ViewController.swift
@@ -8,8 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,FSPagerViewDataSource,FSPagerViewDelegate {
 
+    fileprivate let imageNames = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg"]
+    fileprivate var numberOfItems = 7
+    
+    @IBOutlet weak var pagerView: FSPagerView! {
+        didSet {
+            self.pagerView.register(FSPagerViewCell.self, forCellWithReuseIdentifier: "cell")
+            self.pagerView.itemSize = .zero
+            pagerView.automaticSlidingInterval = 3.0
+            pagerView.isInfinite = true
+        }
+    }
+    
+    @IBOutlet weak var pageControl: FSPageControl! {
+        didSet {
+            self.pageControl.numberOfPages = self.imageNames.count
+            self.pageControl.contentHorizontalAlignment = .right
+            self.pageControl.contentInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +39,51 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    // MARK:- FSPagerView DataSource
+    
+    public func numberOfItems(in pagerView: FSPagerView) -> Int {
+        return self.numberOfItems
+    }
+    
+    public func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
+        let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "cell", at: index)
+        cell.imageView?.image = UIImage(named: self.imageNames[index])
+        cell.imageView?.contentMode = .scaleAspectFill
+        cell.imageView?.clipsToBounds = true
+        cell.textLabel?.text = index.description+index.description
+        return cell
+    }
+    
+    // MARK:- FSPagerView Delegate
+    
+    func pagerView(_ pagerView: FSPagerView, didSelectItemAt index: Int) {
+        pagerView.deselectItem(at: index, animated: true)
+        pagerView.scrollToItem(at: index, animated: true)
+        self.pageControl.currentPage = index
+    }
+    
+    func pagerViewDidScroll(_ pagerView: FSPagerView) {
+        guard self.pageControl.currentPage != pagerView.currentIndex else {
+            return
+        }
+        self.pageControl.currentPage = pagerView.currentIndex // Or Use KVO with property "currentIndex"
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        switch sender.tag {
+        case 1:
+            let newScale = 0.5+CGFloat(sender.value)*0.5 // [0.5 - 1.0]
+            self.pagerView.itemSize = self.pagerView.frame.size.applying(CGAffineTransform(scaleX: newScale, y: newScale))
+        case 2:
+            self.pagerView.interitemSpacing = CGFloat(sender.value) * 20 // [0 - 20]
+        case 3:
+            self.numberOfItems = Int(roundf(sender.value*7.0))
+            self.pageControl.numberOfPages = self.numberOfItems
+            self.pagerView.reloadData()
+        default:
+            break
+        }
+    }
 }
-
